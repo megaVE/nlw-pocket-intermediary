fbd63bce443043651d0abe8257117206b1502b48
megaVE <vinicius.honorio@polygonsolucoes.com.br>
2024-09-13 11:54:17 -0300

    Segunda Aula

--- backend/src/http/server.ts
+++ backend/src/http/server.ts
@@ -4,58 +4,26 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
-import { createGoal } from "../functions/create-goal";
-import { z } from "zod";
-import { getWeekPendingGoals } from "../functions/get-week-pending-goals";
-import { CreateGoalCompletion } from "../functions/create-goal-completion";
+import { createGoalRoute } from "./routes/create-goal";
+import { createCompletionRoute } from "./routes/create-completion";
+import { createGetPendingGoalsRoute } from "./routes/get-pending-goals";
+import { createGetWeekSummaryRoute } from "./routes/get-week-summary";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
 
 // Add schema validator and serializer
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.post(
-  "/goals",
-  {
-    schema: {
-      body: z.object({
-        title: z.string(),
-        desiredWeeklyFrequency: z.number().int().min(1).max(7),
-      }),
-    },
-  },
-  async (request) => {
-    const { body } = request;
-
-    await createGoal({
-      ...body,
-    });
-  }
-);
-
-app.get("/pending-goals", async () => {
-  const { pendingGoals } = await getWeekPendingGoals();
-
-  return { pendingGoals };
-});
-
-app.post(
-  "/completions",
-  {
-    schema: {
-      body: z.object({
-        goalId: z.string(),
-      }),
-    },
-  },
-  async (request) => {
-    const { goalId } = request.body;
-
-    await CreateGoalCompletion({
-      goalId,
-    });
-  }
-);
+// Routes
+app.register(createGoalRoute);
+app.register(createCompletionRoute);
+app.register(createGetPendingGoalsRoute);
+app.register(createGetWeekSummaryRoute);
 
 app.listen({ port: 3333 });
---
+++ frontend/src/components/summary.tsx
@@ -0,0 +1,81 @@
+import { CheckCircle2, Plus } from "lucide-react";
+import { Button } from "./ui/button";
+import { DialogTrigger } from "./ui/dialog";
+import { InOrbitIcon } from "./in-orbit-icon";
+import { Progress, ProgressIndicator } from "./ui/progress-bar";
+import { Separator } from "./ui/separator";
+import { OutlineButton } from "./ui/outline-button";
+
+export function Summary() {
+  return (
+    <div className="py-10 max-w-[30rem] px-5 mx-auto flex flex-col gap-6">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <InOrbitIcon />
+          <span className="text-lg font-semibold">5 a 10 de Agosto</span>
+        </div>
+
+        <DialogTrigger asChild>
+          <Button size="sm">
+            <Plus className="size-4" />
+            Cadastrar meta
+          </Button>
+        </DialogTrigger>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <Progress>
+          <ProgressIndicator style={{ width: 200 }} />
+        </Progress>
+
+        <div className="flex items-center justify-between text-xs text-zinc-400">
+          <span>
+            Você completou <span className="text-zinc-100">X</span> de{" "}
+            <span className="text-zinc-100">Y</span> meta(s) nessa semana.
+          </span>
+          <span>99%</span>
+        </div>
+      </div>
+
+      <Separator />
+
+      <div className="flex flex-wrap gap-3">
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Meditar
+        </OutlineButton>
+
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Meditar2
+        </OutlineButton>
+
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Meditar3
+        </OutlineButton>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <h2 className="text-xl font-medium">Sua semana</h2>
+
+        <div className="flex flex-col gap-4">
+          <h3 className="font-medium">
+            Dia da semana{" "}
+            <span className="text-zinc-400 text-xs">(31 de Fevereiro)</span>
+          </h3>
+
+          <ul className="flex flex-col gap-3">
+            <li className="flex items-center gap-2">
+              <CheckCircle2 className="size-4 text-pink-500" />
+              <span className="text-sm text-zinc-400">
+                Você completou "<span className="text-zinc-100">objetivo</span>"
+                às <span className="text-zinc-100">xx:xxh</span>
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
